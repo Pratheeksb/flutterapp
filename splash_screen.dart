5e58e268197e6270cdf54ac9109ae184693342da
Pratheeksb <47257143+Pratheeksb@users.noreply.github.com>
2020-01-29 22:15:36 +0530

    Add files via upload

---
+++ splash_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:airplane/main.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+
+class splash_screen extends StatefulWidget {
+  splash_screen({Key key}) : super(key: key);
+
+  @override
+  _splash_screenState createState() => _splash_screenState();
+}
+
+class _splash_screenState extends State<splash_screen> {
+
+  void initState() {
+    super.initState();
+    @override
+    Widget build(BuildContext context) {}
+  }
+
+}
